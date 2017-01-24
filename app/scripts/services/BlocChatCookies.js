75da98c5b263473405152de06946251222c630ff
Bennet11 <bennetveloya@gmail.com>
2017-01-24 15:16:08 -0800

    bloc chat 5 complete

--- app/scripts/services/BlocChatCookies.js
+++ app/scripts/services/BlocChatCookies.js
@@ -2,19 +2,17 @@
   function BlocChatCookies($cookies, $uibModal) {
     var currentUser = $cookies.get('blocChatCurrentUser');
     if (!currentUser || currentUser === '') {
-      this.createUser = function () {
-        var usermodal = $uibModal.open({
-          templateUrl: 'templates/user.html',
-          controllerAs: 'usermodalInstance',
-          controller: function($uibModalInstance) {
-            this.addUser = function() {
-              $cookies.put('blocChatCurrentUser', this.user)
-              $uibModalInstance.close();
+      $uibModal.open({
+        templateUrl: 'templates/user.html',
+        controllerAs: 'usermodalInstance',
+        controller: function($uibModalInstance) {
+          this.addUser = function(user) {
+            $cookies.put('blocChatCurrentUser', user)
+            $uibModalInstance.close();
             }
           },
           size: 'md'
-        });
-      }
+      });
     }
   }
 
