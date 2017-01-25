d06600a05d7a3464b883941b51b196fe85fa1743
Bennet11 <bennetveloya@gmail.com>
2017-01-25 15:58:40 -0800

    bloc_chat_6 complete

--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -1,16 +1,23 @@
 (function() {
-  function HomeCtrl(Room, Message) {
+  function HomeCtrl(Room, Message, $cookies) {
     this.rooms = Room.all;
     this.activeRoom = null;
+    this.currentUser = $cookies.get('blocChatCurrentUser');
 
     this.setCurrentRoom = function(room) {
       this.activeRoom = room;
       this.roomName = room.name;
       this.messages = Message.getByRoomId(this.activeRoom.$id);
     }
+
+    this.sendMessage = function () {
+      this.newMessage.roomId = this.activeRoom.$id;
+      this.newMessage.user = this.currentUser;
+      Message.send(this.newMessage)
+    }
  	}
 
   angular
     .module('blocChat')
-    .controller('HomeCtrl', ['Room', 'Message', HomeCtrl]);
+    .controller('HomeCtrl', ['Room', 'Message', '$cookies', HomeCtrl]);
 })();
--- dist/scripts/services/Message.js
+++ dist/scripts/services/Message.js
@@ -2,11 +2,15 @@
   function Message($firebaseArray) {
     var ref = firebase.database().ref().child("messages");
     var messages = $firebaseArray(ref);
+    Message.all = messages;
 
     return {
       getByRoomId: function (roomId) {
         console.log(roomId)
         return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
+      },
+      send: function(newMessage) {
+        messages.$add(newMessage);
       }
     };
     return messages
